0c228ef613c96ecbd9d51e9a0d4ea17e8f9b3096
Jeff <gef.nyangoya@gmail.com>
2022-12-18 11:37:59 +0300

    The if statement

---
+++ ifStatement.c
@@ -0,0 +1,11 @@
+//Use if to specify a block of code to be executed,if a specified condition is true
+#iclude <stdio.h>
+  int main()
+{
+	int x = 20;
+	int y = 18;
+	if(x>y){
+		printf("x is greater than y");
+	}
+	return 0;
+}
